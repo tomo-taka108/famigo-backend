b8b74aa45392d6ab6fd3c436460264eaf905d6f4
tomo-taka108 <ikenocity@gmail.com>
2025-11-17 19:14:42 +0900

    Initial commit

---
+++ src/test/java/com/famigo/backend/FamigoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.famigo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamigoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
